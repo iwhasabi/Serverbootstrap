ed463b7a655786f40cc52955c58dfeb3cc947284
iWhasabi <jmartincarratala@outlook.es>
2021-11-29 14:08:38 +0100

    first commit

---
+++ src/test/java/com/everis/microservices/Microservicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
